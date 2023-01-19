376a98d31aadf53cec6ecc801e68862267ee6d55
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-01-19 10:52:51 -0600

    fontFamily personalizado con fuente del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
       boxShadowColor: {
         "strong-cyan": "hsl(171, 66%, 44%)",
         "light-blue": "hsl(233, 100%, 69%)",
+      },
+      fontFamily: {
+        "bai": "Bai Jamjuree', sans-serif"
       }
     },
   },
